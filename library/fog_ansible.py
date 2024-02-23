56b0062d715542ab7f01afb624072d7137941261
TimoFeddern <timo@feddern.dev+>
2024-02-23 15:03:31 +0100

    added task creation capabilities

--- library/fog_ansible.py
+++ library/fog_ansible.py
@@ -5,54 +5,37 @@ import json
 import logging
 import collections
 
-def test_api_connection(url, fog_api_token, fog_user_token, timeout=10):
-    url = url.rstrip("/") + "/system/status"  # Ensure the URL ends with a single forward slash
-    payload = {}
+def make_request(
+        method: str,
+        url: str,
+        fog_api_token: str,
+        fog_user_token: str,
+        payload: dict = None,
+        timeout: int = 10
+        ) -> (dict[str, str]):
+    #url = url.rstrip("/")  # Ensure the URL ends with a single forward slash
     headers = {
         'fog-user-token': fog_user_token,
-        'fog-api-token': fog_api_token
+        'fog-api-token': fog_api_token,
+        'Content-Type': 'application/json'  # Assuming JSON payload
     }
 
     try:
-        response = requests.get(url, headers=headers, data=payload, timeout=timeout)
-        if response.status_code == 200:
-            return "Successful!"
-        else:
-            return "Unsuccessful!"
-
-    except requests.exceptions.Timeout:
-        logging.error(f"Request to {url} timed out after {timeout} seconds.")
-        raise
-
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Error while making request to {url}: {e}")
-        raise
-
-    except ValueError as ve:
-        logging.error(ve)
-        raise
-
-def get_hosts(url, fog_api_token, fog_user_token, timeout=10):
-    url = url.rstrip("/") + "/host"  # Ensure the URL ends with a single forward slash
-    payload = {}
-    headers = {
-        'fog-user-token': fog_user_token,
-        'fog-api-token': fog_api_token
-    }
-
-    try:
-        response = requests.get(url, headers=headers, data=payload, timeout=timeout)
-
-        # Check for HTTP errors
+        response = requests.request(method, url, headers=headers, json=payload, timeout=timeout)
         response.raise_for_status()
 
-        # Check for API-level errors
-        response_json = response.json()
-        if response_json.get('error'):
-            raise ValueError(f"Fog API error: {response_json['error']['message']}")
-
-        return response_json
-
+        if response.text == 'success\n':
+            response_json = {"status": "success"}
+            return response_json
+        try:
+            response_json = response.json()
+            if str(response_json) == "" or response_json == None or response_json == "null" or response_json == "None" or response_json == "none" or response_json == "NULL":
+                response_json = {"status": "success"}
+            return response_json
+        except:
+            response_json = {"status": "success"}
+            return response_json
+        
     except requests.exceptions.Timeout:
         logging.error(f"Request to {url} timed out after {timeout} seconds.")
         raise
@@ -65,46 +48,47 @@ def get_hosts(url, fog_api_token, fog_user_token, timeout=10):
         logging.error(ve)
         raise
 
-def get_groups(url, fog_api_token, fog_user_token, timeout=10):
-    url = url.rstrip("/") + "/group"  # Ensure the URL ends with a single forward slash
-    payload = {}
-    headers = {
-        'fog-user-token': fog_user_token,
-        'fog-api-token': fog_api_token
-    }
+def test_api_connection(
+        url: str,
+        fog_api_token: str,
+        fog_user_token: str,
+        timeout: int
+        )-> (dict[str, str]):
+    url = url.rstrip("/") + "/system/status"
+    return make_request("GET", url, fog_api_token, fog_user_token, timeout)
 
-    try:
-        response = requests.get(url, headers=headers, data=payload, timeout=timeout)
+def _get_hosts(
+        url: str, 
+        fog_api_token: str, 
+        fog_user_token: str,
+        timeout: int
+        ):
+    url = f"{url}/host"
+    return make_request("GET", url, fog_api_token, fog_user_token, timeout)
 
-        # Check for HTTP errors
-        response.raise_for_status()
+def _get_groups(
+        url: str, 
+        fog_api_token: str, 
+        fog_user_token: str,
+        timeout
+        ):
+    url = f"{url}/group"
+    return make_request("GET", url, fog_api_token, fog_user_token, timeout)
 
-        # Check for API-level errors
-        response_json = response.json()
-        if response_json.get('error'):
-            raise ValueError(f"Fog API error: {response_json['error']['message']}")
-
-        return response_json
-
-    except requests.exceptions.Timeout:
-        logging.error(f"Request to {url} timed out after {timeout} seconds.")
-        raise
-
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Error while making request to {url}: {e}")
-        raise
-
-    except ValueError as ve:
-        logging.error(ve)
-        raise
-
-def list_hosts(url, fog_api_token, fog_user_token, output='minimal'):
-    hosts_list = get_hosts(url, fog_api_token, fog_user_token)
+def list_hosts(
+        url: str, 
+        fog_api_token: str, 
+        fog_user_token: str, 
+        output,
+        timeout
+        ):
+    
+    hosts_list = _get_hosts(url, fog_api_token, fog_user_token, timeout)
     hosts_fancy_list = {}
     hosts_fancy_list["Connection successful, hosts count:"] = {hosts_list["count"]}
     hosts_fancy_list["Output type"] = f"{output}"
 
-    if output == 'minimal':
+    if output == 'minimal' or output is None:
         return hosts_fancy_list
     
     elif output == 'full':
@@ -119,14 +103,20 @@ def list_hosts(url, fog_api_token, fog_user_token, output='minimal'):
     else:
         logging.error(f"Unknown output type '{output}'")
         raise
-
-def list_groups(url, fog_api_token, fog_user_token, output='minimal'):
-    group_list = get_groups(url, fog_api_token, fog_user_token)
+    
+def list_groups(
+        url: str, 
+        fog_api_token: str, 
+        fog_user_token: str, 
+        output,
+        timeout: int = 10
+        ):
+    group_list = _get_groups(url, fog_api_token, fog_user_token, timeout)
     group_fancy_list = {}
-    group_fancy_list["Connection successful, hosts count:"] = {group_list["count"]}
+    group_fancy_list["Connection successful, groups count:"] = {group_list["count"]}
     group_fancy_list["Output type"] = f"{output}"
 
-    if output == 'minimal':
+    if output == 'minimal' or output is None:
         return group_fancy_list
     
     elif output == 'full':
@@ -142,55 +132,123 @@ def list_groups(url, fog_api_token, fog_user_token, output='minimal'):
         logging.error(f"Unknown output type '{output}'")
         raise
 
-def add_hosts_to_group(url, fog_api_token, fog_user_token, group_name, hosts):
+def add_hosts_to_group(
+        url: str, 
+        fog_api_token: str, 
+        fog_user_token: str, 
+        group_name: str,
+        hosts: list[str] = None
+        )-> (dict[str, str]):
     # Implement your logic here
     pass
 
-#def create_group(url, fog_api_token, fog_user_token, group_name = "ABCD", timeout=10):
-def create_group(url, fog_api_token, fog_user_token, group_name):
-    url = f"{url.rstrip('/')}/group/create"
-    payload = {"name": group_name, "description": "Group created by Ansible", }
-    headers = {
-        'fog-user-token': fog_user_token,
-        'fog-api-token': fog_api_token,
-        'Content-Type': 'application/json'  # Assuming JSON payload
-    }
+def _get_group_id(
+        url: str, 
+        fog_api_token: str, 
+        fog_user_token: str, 
+        group_name: str, 
+        timeout: int
+        ):
+    
+    group_list = _get_groups(url, fog_api_token, fog_user_token, timeout)
 
+    for group in group_list["groups"]:
+        if str(group['name']) == str(group_name):
+            return group['id']
+        else:
+            pass
+    
+    raise ValueError(f"Group '{group_name}' not found")
+
+def _get_host_id(
+        url: str, 
+        fog_api_token: str, 
+        fog_user_token: str, 
+        host_name: str, 
+        timeout: int
+        ):
+    
+    hosts_list = _get_hosts(url, fog_api_token, fog_user_token, timeout)
+
+    for host in hosts_list["groups"]:
+        if str(host['name']) == str(host_name):
+            return host['id']
+        else:
+            pass
+    
+    raise ValueError(f"Host '{host_name}' not found")
+
+def create_group(
+        url: str, 
+        fog_api_token: str, 
+        fog_user_token: str, 
+        group_name: str,
+        timeout: int
+        ) -> (dict[str, str]):
+    url = f"{url}/group/create"
+    payload = {"name": group_name, "description": "Group created by Ansible"}
+    return make_request("POST", url, fog_api_token, fog_user_token, payload, timeout)
+
+def delete_group(
+        url: str, 
+        fog_api_token: str, 
+        fog_user_token: str, 
+        group_name: str, 
+        timeout
+        ) -> dict:
+    id = _get_group_id(url, fog_api_token, fog_user_token, group_name, timeout)
+    url = f"{url}/group/{id}/delete"
+    response = make_request("DELETE", url, fog_api_token, fog_user_token, None, timeout)
+    
     try:
-        response = requests.post(url, headers=headers, json=payload)
-        response.raise_for_status()
+        if response["status"] == "success":
+            return {"status": "success", "message": f"Group '{group_name}' with id '{id}' deleted"}
+    except:
+        return response
 
-        response_json = response.json()
-        if response_json.get('error'):
-            raise ValueError(f"Fog API error: {response_json['error']['message']}")
+def create_task_group(
+        url: str,
+        fog_api_token: str,
+        fog_user_token: str,
+        task_name: str, 
+        group_name: str, 
+        task_type: str, 
+        timeout: int
+        ) -> (dict[str, str]):
+    id = _get_group_id(url, fog_api_token, fog_user_token, group_name, timeout)
+    url = f"{url}/group/{id}/task"
+    if task_type == "multicast":
+        taskTypeID = 8
+    else:
+        raise ValueError(f"Unknown or not yet implemented task type '{task_type}'")
+    payload = {
+    "taskTypeID": taskTypeID,
+    "wol": 1,
+    "name": task_name
+    }
+    return make_request("POST", url, fog_api_token, fog_user_token, payload, timeout)
 
-        response_output = {
-            "Group created": group_name,
-            "Group ID": response_json['id']
-        }
-
-        return response_output
-
-    except requests.exceptions.Timeout:
-        logging.error(f"Request to {url} timed out.")
-        raise
-
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Error making request to {url}: {e}")
-        raise
-
-    except ValueError as ve:
-        logging.error(ve)
-        raise
-
-    except Exception as ex:
-        logging.error(f"Unknown error: {ex}")
-        raise
-
-
-def create_task(url, fog_api_token, fog_user_token, task_name, multicast_targets):
-    # Implement your logic here
-    pass
+def create_task_host(
+        url: str,
+        fog_api_token: str,
+        fog_user_token: str,
+        task_name: str,
+        host_name: str,
+        task_type: str,
+        timeout: int
+        ) -> (dict[str, str]):
+    id = _get_host_id(url, fog_api_token, fog_user_token, host_name, timeout)
+    url = f"{url}/host/{id}/task"
+    if task_type == "multicast":
+        taskTypeID = 8
+    else:
+        raise ValueError(f"Unknown or not yet implemented task type '{task_type}'")
+    payload = {
+    "taskTypeID": taskTypeID,
+    "wol": 1,
+    "name": task_name
+    }
+    return make_request("POST", url, fog_api_token, fog_user_token, payload, timeout)
 
 def list_tasks(url, fog_api_token, fog_user_token):
     # Implement your logic here
@@ -204,6 +262,8 @@ def main():
         action=dict(type='str', required=True),
         output=dict(type='str', required=False),
         group_name=dict(type='str', required=False),
+        timeout=dict(type='int', required=False, default=10),
+        host_name=dict(type='str', required=False),
         # Add other required parameters for specific actions
     )
 
@@ -223,17 +283,29 @@ def main():
     fog_user_token = module.params['fog_user_token']
     action = module.params['action']
     output = module.params['output']
+    if output is not type(str):
+        output = "minimal"
     group_name = module.params['group_name']
+    timeout = module.params['timeout']
+    if timeout is None or timeout == '' or timeout == "null":
+        timeout = 10
+    host_name = module.params['host_name']
 
     # Implement logic based on the specified action
     if action == 'test_api_connection':
-        result['test_connection'] = test_api_connection(url, fog_api_token, fog_user_token)
+        result['test_connection'] = test_api_connection(url, fog_api_token, fog_user_token, timeout)
     elif action == 'list_hosts':
-        result['hosts'] = list_hosts(url, fog_api_token, fog_user_token, output)
+        result['hosts'] = list_hosts(url, fog_api_token, fog_user_token, output, timeout)
     elif action == 'list_groups':
-        result['groups'] = list_groups(url, fog_api_token, fog_user_token, output)
+        result['groups'] = list_groups(url, fog_api_token, fog_user_token, output, timeout)
     elif action == 'create_group':
-        result['group'] = create_group(url, fog_api_token, fog_user_token, group_name)
+        result['group'] = create_group(url, fog_api_token, fog_user_token, group_name, timeout)
+    elif action == 'delete_group':
+        result['group'] = delete_group(url, fog_api_token, fog_user_token, group_name, timeout)
+    elif action == 'create_task_group':
+        result['task'] = create_task_group(url, fog_api_token, fog_user_token, group_name, timeout)
+    elif action == 'create_task_host':
+        result['task'] = create_task_host(url, fog_api_token, fog_user_token, host_name, timeout)
     # Add other action cases...
     else:
         module.fail_json(f"Unknown action '{action}'")
